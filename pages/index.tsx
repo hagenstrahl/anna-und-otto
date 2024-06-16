3598b665147084815302d2bd7e438bae4bc2e109
behindadyingstar <133813077+behindadyingstar@users.noreply.github.com>
2024-06-16 22:03:33 +0200

    English index.tsx
    english version of the text

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,14 @@ const Home = (): JSX.Element => {
   return (
     <Layout heading="Coming Soon!">
       <section className="max-w-3xl text-center">
-        Ob wärmende Strahlen der ersten Frühlingssonne oder bewölkter Himmel mit
-        Nieselregen – ab jetzt gibt es mindestens einen guten Grund, auch bei
-        Schietwetter vor die Tür zu gehen. Das Ziel: unser süßes, gemütliches
-        Café am Neustrelitzer Markt. Bei ANNA & OTTO findet ihr endlich wieder
-        Zeit für die kleinen Dinge des Lebens. Duftender, selbst gebackener
-        Kuchen, Gespräche über die kleinen und großen Wunder des Alltags oder
-        ausgiebiges Faxen machen in unserer großen Spielecke.
+        Whether it's the warming rays of the first spring sun 
+        or a cloudy sky with drizzle – from now on, there is at 
+        least one good reason to go outside even in lousy weather. 
+        The destination: our sweet, cozy café at the Neustrelitz market. 
+        At ANNA & OTTO, you'll finally find time 
+        for the little things in life again. Fragrant, homemade cake, 
+        conversations about the small and big wonders of everyday life, 
+        or extensive fun in our large play area.
       </section>
       <section className="max-w-3xl mt-5 text-center">
         Übrigens: In einem separaten Beratungs- und Seminarraum erfahrt ihr
